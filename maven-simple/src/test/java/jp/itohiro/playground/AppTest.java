ce8f19d6137263a8b0a1a735a277fd82d147d11c
Hiroshi Ito <itohiro73@gmail.com>
2015-12-28 23:16:56 +0900

    Add maven-simple module.

---
+++ maven-simple/src/test/java/jp/itohiro/playground/AppTest.java
@@ -0,0 +1,19 @@
+package jp.itohiro.playground;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        Assert.assertTrue( true );
+    }
+}
