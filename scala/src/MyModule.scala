f5662de815c278b0f9af8905dae219005180944d
Hiroshi Ito <itohiro73@gmail.com>
2016-02-01 22:24:53 +0900

    FP in Scala, Part2.

---
+++ scala/src/MyModule.scala
@@ -0,0 +1,29 @@
+object MyModule {
+
+  def factorial(n: Int): Int = {
+    def go(n: Int, acc: Int): Int = {
+      if(n <= 0) acc
+      else go(n-1, n*acc)
+    }
+
+    go(n, 1)
+  }
+
+  def abs(n: Int): Int =
+    if (n < 0) -n
+    else n
+
+  private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
+  }
+
+  private def formatFactorial(x: Int) = {
+    val msg = "The factorial of %d is %d"
+    msg.format(x, factorial(x))
+  }
+
+  def main(args: Array[String]): Unit =
+    println(formatAbs(-42))
+    println(formatFactorial(7))
+}
