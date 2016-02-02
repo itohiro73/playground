ea525df7c37b8880132a51d172260b0b8f9c322a
Hiroshi Ito <itohiro73@gmail.com>
2016-02-02 22:15:22 +0900

    FP in Scala, Chapter2, add fibonacci function.

--- scala/src/MyModule.scala
+++ scala/src/MyModule.scala
@@ -1,5 +1,14 @@
 object MyModule {
 
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def loop(n: Int, prev: Int, cur: Int): Int = {
+      if(n==0) prev
+      else loop(n-1, cur, prev + cur)
+    }
+    loop(n, 0, 1)
+  }
+
   def factorial(n: Int): Int = {
     def go(n: Int, acc: Int): Int = {
       if(n <= 0) acc
@@ -13,17 +22,12 @@ object MyModule {
     if (n < 0) -n
     else n
 
-  private def formatAbs(x: Int) = {
-    val msg = "The absolute value of %d is %d"
-    msg.format(x, abs(x))
-  }
-
-  private def formatFactorial(x: Int) = {
-    val msg = "The factorial of %d is %d"
-    msg.format(x, factorial(x))
+  def formatResult(s: String, i: Int, func: (Int) => Int): String = {
+    val msg = "The %s of %d is %d"
+    msg.format(s, i, func(i))
   }
 
   def main(args: Array[String]): Unit =
-    println(formatAbs(-42))
-    println(formatFactorial(7))
+    println(formatResult("absolute value", -42, abs))
+    println(formatResult("factorial", 7, factorial))
 }
